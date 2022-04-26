17aa492abc4edc8676915d8c9237b4e9b41bc90d
Tatiana Kushnir <tatianakushnir@Tatianas-MacBook-Pro.local>
2022-04-26 16:19:32 -0700

    added newgame, init, code refactore

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 'use strict';
+// Selecting elements
 
 let rollDiceBtn = document.querySelector('.btn--roll');
 let dice = document.querySelector('.dice');
 let currentScore = document.querySelectorAll('.current-score');
 let player = document.querySelectorAll('.player');
 let holdBtn = document.querySelector('.btn--hold');
+let newGameBtn = document.querySelector('.btn--new');
 let score = document.querySelectorAll('.score');
-let diceRendomNumber = 0;
-let diceRendomNumberSum = 0;
-let scoreSumArray = [0, 0];
-let currentPlayer = 0;
-let getRendonNumber = function () {
-  return (diceRendomNumber = Math.floor(Math.random() * 6) + 1);
+
+let diceRendomNumber,
+  diceRendomNumberSum,
+  scoreSumArray,
+  currentPlayer,
+  playing;
+
+let init = function () {
+  diceRendomNumber = 0;
+  diceRendomNumberSum = 0;
+  scoreSumArray = [0, 0];
+  currentPlayer = 0;
+  playing = true;
+  dice.classList.add('hidden');
+
+  player[0].classList.remove('player--winner');
+  player[1].classList.remove('player--winner');
+  player[0].classList.add('player--active');
+  player[1].classList.remove('player--active');
+  for (let i = 0; i < player.length; i++) {
+    currentScore[i].textContent = 0;
+    score[i].textContent = 0;
+  }
 };
+init();
+
 let activateCurrentPlayer = function (currentPlayer) {
   if (currentPlayer === 1) {
     player[0].classList.remove('player--active');
@@ -24,37 +45,44 @@ let activateCurrentPlayer = function (currentPlayer) {
 };
 
 rollDiceBtn.addEventListener('click', function () {
-  getRendonNumber();
-  diceRendomNumberSum = diceRendomNumberSum + diceRendomNumber;
-  dice.classList.remove('hidden');
-  dice.setAttribute('src', `dice-${diceRendomNumber}.png`);
-  activateCurrentPlayer(currentPlayer);
-
-  if (diceRendomNumber === 1) {
-    currentPlayer === 0 ? (currentPlayer = 1) : (currentPlayer = 0);
+  if (playing) {
+    diceRendomNumber = Math.floor(Math.random() * 6) + 1;
+    diceRendomNumberSum = diceRendomNumberSum + diceRendomNumber;
+    dice.classList.remove('hidden');
+    dice.setAttribute('src', `dice-${diceRendomNumber}.png`);
     activateCurrentPlayer(currentPlayer);
-    diceRendomNumberSum = 0;
-  }
-  for (let i = 0; i < player.length; i++) {
-    if (player[i].classList.contains('player--active')) {
-      currentScore[i].textContent = diceRendomNumberSum;
-    } else {
-      currentScore[i].textContent = 0;
+
+    if (diceRendomNumber === 1) {
+      currentPlayer = currentPlayer === 0 ? 1 : 0;
+      activateCurrentPlayer(currentPlayer);
+      diceRendomNumberSum = 0;
+    }
+    for (let i = 0; i < player.length; i++) {
+      if (player[i].classList.contains('player--active')) {
+        currentScore[i].textContent = diceRendomNumberSum;
+      } else {
+        currentScore[i].textContent = 0;
+      }
     }
   }
 });
 
 holdBtn.addEventListener('click', function () {
-  scoreSumArray[currentPlayer] =
-    scoreSumArray[currentPlayer] + diceRendomNumberSum;
-  score[currentPlayer].textContent = scoreSumArray[currentPlayer];
+  if (playing) {
+    scoreSumArray[currentPlayer] =
+      scoreSumArray[currentPlayer] + diceRendomNumberSum;
+    score[currentPlayer].textContent = scoreSumArray[currentPlayer];
 
-  if (scoreSumArray[currentPlayer] >= 100) {
-    player[currentPlayer].classList.add('player--winner');
-    dice.classList.add('hidden');
-    currentScore[currentPlayer].textContent = diceRendomNumberSum;
+    if (scoreSumArray[currentPlayer] >= 20) {
+      playing = false;
+      player[currentPlayer].classList.add('player--winner');
+      dice.classList.add('hidden');
+      currentScore[currentPlayer].textContent = diceRendomNumberSum;
+    }
+    currentPlayer = currentPlayer === 0 ? 1 : 0;
+    diceRendomNumberSum = 0;
+    activateCurrentPlayer(currentPlayer);
   }
-  currentPlayer === 0 ? (currentPlayer = 1) : (currentPlayer = 0);
-  diceRendomNumberSum = 0;
-  activateCurrentPlayer(currentPlayer);
 });
+
+newGameBtn.addEventListener('click', init);
